b190d51f8336212daee8a89f1c96afbe38065d84
MrGuptaRanjit <ranjitkumar22129cse@sdbc.ac.in>
2025-11-10 23:02:42 +0530

    Added routes and controllers folder, updated server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,20 +5,13 @@ import express from "express";
 // we can do this like 
 import "dotenv/config";
 
+import authRoutes from "./routes/auth.routes.js";
+
 const app = express();
 const PORT = process.env.PORT
 
-app.get("/api/auth/signup" ,(req,res)=>{
-    res.send("SignUp Route");
-});
 
-app.get("/api/auth/login" ,(req,res)=>{
-    res.send("login Route");
-});
-
-app.get("/api/auth/logout" ,(req,res)=>{
-    res.send("logout Route");
-});
+app.use("/api/auth", authRoutes)
 
 app.listen(PORT, ()=>{
     console.log(`Server is running on port ${PORT}`);
